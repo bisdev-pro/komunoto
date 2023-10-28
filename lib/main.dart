896a81d44593bf29026243c4c55a69ab02f51654
bisdev-pro <76934794+bisdev-pro@users.noreply.github.com>
2023-10-28 15:08:40 +0700

    Ganti Title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Komunako',
       theme: ThemeData(
         // This is the theme of your application.
         //
