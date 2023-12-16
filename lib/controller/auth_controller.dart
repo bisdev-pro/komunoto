86107d1123754aa9be312e4daaa5b796e52325b9
Febry <programmer@servvo.co.id>
2023-12-16 21:59:56 +0700

    Refactor postLogin and postRegistData methods

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -14,40 +14,64 @@ class AuthController {
 
   AuthController(this.context);
 
-  void postLogin(String phoneNumber, String uid) async {
-    Map<String, dynamic> data = {
-      'auth': phoneNumber,
-      'provider': 5,
-      'uuid': uid,
-    };
-    http.Response response = await postApiLoginPhone(data);
-    print(response.statusCode);
-    // Now you can use the response
-    if (response.statusCode == 200) {
-      onLoginSuccess();
-    } else if (response.statusCode == 404) {
-      // If the server returns an unsuccessful response code,
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: ((context) => HomeScreen())));
+  void postLogin(String? phoneNumber, String uid, String? email, String? provider) async {
+    if (phoneNumber != null) {
+      Map<String, dynamic> data = {
+        'auth': phoneNumber,
+        'provider': 5,
+        'uuid': uid,
+      };
+      http.Response response = await postApiLoginPhone(data);
+      print(response.statusCode);
+      // Now you can use the response
+      if (response.statusCode == 200) {
+        onLoginSuccess();
+      } else if (response.statusCode == 404) {
+        // If the server returns an unsuccessful response code,
+        postRegistData(email, phoneNumber, uid);
+        // Navigator.pushReplacement(
+        //     context, MaterialPageRoute(builder: ((context) => HomeScreen())));
+      } else {
+        // If the server returns an unsuccessful response code,
+        BarError.showSnackBar('Check your internet connection');
+      }
     } else {
-      // If the server returns an unsuccessful response code,
-      BarError.showSnackBar('Check your internet connection');
+      Map<String, dynamic> data = {
+        'auth': email,
+        'provider': 5,
+        'uuid': uid,
+      };
+      http.Response response = await postApiLoginPhone(data);
+      print(response.statusCode);
+      // Now you can use the response
+      if (response.statusCode == 200) {
+        onLoginSuccess();
+      } else if (response.statusCode == 404) {
+        // If the server returns an unsuccessful response code,
+        postRegistData(email, phoneNumber, uid);
+      } else {
+        // If the server returns an unsuccessful response code,
+        BarError.showSnackBar('Check your internet connection');
+      }
     }
   }
 
-  void postRegistData(String phoneNumber) async {
+  void postRegistData(String? phoneNumber, String? email, String? uid) async {
     Map<String, dynamic> data = {
-      'phone': phoneNumber,
-      'service': 0,
+      // 'phone': phoneNumber,
+      // 'service': 0,
+      "email": "email@email.com", // kosong kalo pake phone number
+      "phoneNumber": "", //kosong kalo register pake email
+      "provider": 2,
+      "uuid": "email@email.com" // jika provider selain 1
     };
     http.Response response = await postApiRegistPhone(data);
     // Dekode response body
     Map<String, dynamic> responseBody = jsonDecode(response.body);
-    print(response.statusCode);
-    print(responseBody['meta']['message']);
     // Now you can use the response
     if (response.statusCode == 200) {
-      onRegistSuccess();
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: ((context) => HomeScreen())));
     } else if (response.statusCode == 404) {
       // If the server returns an unsuccessful response code,
       BarError.showSnackBar('Failed To Register');
