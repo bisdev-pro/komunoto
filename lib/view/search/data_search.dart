899131c6bc0ddda1fecfb355f0468bd7c278601e
Febry <it@komunoto.com>
2023-12-25 22:53:55 +0700

    Update clear icon in DataSearch

--- lib/view/search/data_search.dart
+++ lib/view/search/data_search.dart
@@ -5,7 +5,7 @@ class DataSearch extends SearchDelegate<String> {
   List<Widget> buildActions(BuildContext context) {
     return [
       IconButton(
-        icon: Icon(Icons.clear),
+        icon: const Icon(Icons.clear),
         onPressed: () {
           query = '';
         },
